84b545344e28619b51966d1a319aa27ec284aa46
Eragon67360 <thomas-moser@orange.fr>
2024-11-18 21:40:22 +0100

    Initial commit

---
+++ src/views/LoginView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="w-screen h-screen flex items-center justify-center">
+<div class="border rounded-lg" ></div>
+
+  </div>
+</template>
+
+<style scoped></style>
